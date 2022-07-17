e490ce168dbb24aecb78c3214b815d9d0a75503a
behnam hesabi <atlasplants@gmail.com>
2022-07-17 18:07:31 +0430

    omid
    first

---
+++ Assets/Scripts/Cutter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+using Sirenix.OdinInspector;
+public class Cutter : MonoBehaviour
+{
+
+    public GameObject objectToSlice; // non-null
+    public Material crossSectionMaterial; // non-null
+    public GameObject Plane;
+    public GameObject p1, p2;
+    public Vector3 vec;
+    /**
+     * Example on how to slice a GameObject in world coordinates.
+     * Uses a custom TextureRegion to offset the UV coordinates of the cross-section
+     * Uses a custom Material
+     */
+    [Button("Cut", ButtonSizes.Medium)]
+
+    public void Cut()
+    {
+        // var dir = p2.transform.position - p1.transform.position;
+        var hull = Slice(Plane.transform.position, Plane.transform.up, crossSectionMaterial);
+
+        hull.CreateLowerHull(objectToSlice, crossSectionMaterial);
+        hull.CreateUpperHull(objectToSlice, crossSectionMaterial);
+        objectToSlice.SetActive(false);
+        // Debug.Log("Cut");
+    }
+    [Button("Angle", ButtonSizes.Medium)]
+
+    public void angle()
+    {
+        var dir = p2.transform.position - p1.transform.position;
+        float angle = Vector3.Angle(dir, vec);
+
+        Debug.Log("angle:" + angle);
+    }
+    public SlicedHull Slice(Vector3 planeWorldPosition, Vector3 planeWorldDirection, Material mat)
+    {
+        return objectToSlice.Slice(planeWorldPosition, planeWorldDirection, mat);
+    }
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+public class TouchController : MonoBehaviour
+{
+
+   // public GameObject PointerObject;
+    public GameObject Plane;
+    public Cutter cutter;
+  //  private Vector3 point;
+    public LayerMask maskBackground;
+    public LineRenderer line;
+    private RaycastHit hit;
+    
+    private Ray ray;
+    private Vector3 currentPos;
+    private Vector3 prev_pos;
+    private Vector3 point1;
+    private Vector3 point2;
+    public Vector3 GuidLine;
+    public void OnMouseDown()
+    {
+        ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, maskBackground))
+        {
+            Plane.transform.position = hit.point;
+            currentPos = new Vector3(hit.point.x, hit.point.y, -4.0f);
+            //Debug.Log("Wall");
+        }
+    }
+    public void OnMouseDrag()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, maskBackground))
+        {
+            //prev_pos = currentPos;
+            //Plane.transform.LookAt(new Vector3(hit.point.x, hit.point.y, 0));
+            Plane.transform.position = new Vector3(hit.point.x, hit.point.y, -4.0f);
+            
+         //   currentPos = new Vector3(hit.point.x, hit.point.y, -4.0f);
+          //  Debug.Log($"Vec : {prev_pos}, Current:{currentPos}");
+        }
+    }
+    public void OnMouseUp()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, maskBackground))
+        {
+
+            prev_pos = new Vector3(hit.point.x, hit.point.y, -4.0f);
+
+            var dir = prev_pos - currentPos;
+
+            float angle = Vector3.Angle(dir, GuidLine);
+
+            Plane.transform.eulerAngles = new Vector3(0, 0, angle);
+            Debug.Log("angle:" + angle);
+        }
+
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, Vector3.zero);
+        //cutter.Cut();
+       // Debug.Log("Cut");
+    }
+    /* public void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            PlanePlace();
+
+        }
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            PlaneRotate();
+
+        }
+
+    }
+   public void PlanePlace()
+    {
+        
+      ///  var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float x = Input.mousePosition.x;
+        float y =  Input.mousePosition.y;
+        point = Camera.main.ScreenToWorldPoint(new Vector3(x, y, Camera.main.nearClipPlane));
+        Plane.transform.position = point;
+    }
+    public void PlaneRotate()
+    {
+        float x = Input.mousePosition.x;
+        float y = Input.mousePosition.y;
+        point = Camera.main.ScreenToWorldPoint(new Vector3(x, y, Camera.main.nearClipPlane));
+
+        Plane.transform.LookAt(point);
+    }*/
+}
